8a2d03d2da5ac1535f8383c19844783b34363117
Luna :3 <vcvtph2ps@proton.me>
2026-05-17 10:55:51 +1000

    minor: add generic interrupt get state function

--- kernel/include/arch/x86_64/arch/interrupt.h
+++ kernel/include/arch/x86_64/arch/interrupt.h
@@ -5,5 +5,20 @@ typedef struct {
     bool enabled;
 } arch_interrupt_state_t;
 
+/**
+ * @brief Gets the current interrupt state
+ * @return The current interrupt state.
+ */
+[[nodiscard]] arch_interrupt_state_t arch_interrupt_get_state();
+
+/**
+ * @brief Disables interrupts and returns the previous interrupt state.
+ * @return The previous interrupt state before disabling.
+ */
 [[nodiscard]] arch_interrupt_state_t arch_interrupt_disable();
+
+/**
+ * @brief Restores the interrupt state to the given state.
+ * @param state The interrupt state to restore to.
+ */
 void arch_interrupt_restore(arch_interrupt_state_t state);
--- kernel/src/arch/x86_64/interrupt.c
+++ kernel/src/arch/x86_64/interrupt.c
@@ -1,6 +1,13 @@
 #include <arch/interrupt.h>
 #include <stdint.h>
 
+[[nodiscard]] arch_interrupt_state_t arch_interrupt_get_state() {
+    uint64_t rflags;
+    __asm__ volatile("pushfq\n" "pop %0\n" : "=r"(rflags) : : "memory");
+    bool enabled = (rflags & (1 << 9)) > 0;
+    return (arch_interrupt_state_t) { .enabled = enabled };
+}
+
 [[nodiscard]] arch_interrupt_state_t arch_interrupt_disable() {
     uint64_t rflags;
     __asm__ volatile("pushfq\n" "pop %0\n" "cli\n" : "=r"(rflags) : : "memory");
